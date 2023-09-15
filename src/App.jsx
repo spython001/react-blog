5cd0b4ad3f577a4cb47b056a5b4e43b33a983ff3
spython <emmanuelegbo01@gmail.com>
2023-09-15 18:48:25 +0100

    added a react router package

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-//import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/topbar";
 import Login from "./pages/login/Login";
-//import Settings from "./pages/settings/Settings";
-//import Single from "./pages/single/Single";
-//import Write from "./pages/write/Write";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <Router>
       <Topbar />
-      <Login />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
