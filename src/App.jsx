d8879ea6eeacef577c61a36e8468271a0de832d0
spython <emmanuelegbo01@gmail.com>
2023-09-15 15:07:23 +0100

    created single post folder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/topbar";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Home />
+      <Single />
     </>
   )
 }
