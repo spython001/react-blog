9bf844f65877401f567c46f5e871914fdda42020
spython <emmanuelegbo01@gmail.com>
2023-09-15 14:54:43 +0100

    created a components folder and moved the blog components there

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,36 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img
+          className="postImg"
+          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+          alt="" 
+        />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            
+            <span className="postTitle">
+                Lorem, ipsum dolor sit amet
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Blanditiis alias fugit nesciunt nulla error architecto beatae, provident accusamus rerum sint facere doloremque, et ea. 
+            Molestias nobis reiciendis asperiores voluptates a!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Blanditiis alias fugit nesciunt nulla error architecto beatae, provident accusamus rerum sint facere doloremque, et ea. 
+            Molestias nobis reiciendis asperiores voluptates a!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Blanditiis alias fugit nesciunt nulla error architecto beatae, provident accusamus rerum sint facere doloremque, et ea. 
+            Molestias nobis reiciendis asperiores voluptates a!
+        </p>
+    </div>
+  )
+}
