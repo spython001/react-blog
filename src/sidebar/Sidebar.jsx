934306e354964f8dec3a76c24ad9605530442e3b
spython <emmanuelegbo01@gmail.com>
2023-09-15 14:12:05 +0100

    added s a sidebar image

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="sidebarItem">
+            <span className="sidebarTitle">ABOUT ME</span>
+            <img 
+              src="/assets/blogSidebar.png" 
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Pariatur qui odio libero, error reiciendis harum et dolor autem!
+            </p>
+        </div>
+        <div className="sideBarItem">
+            <span className="sidebarTitle">CATEGORIES</span>
+            <ul className="sidebarList">
+                <li className="sidebarListItem">Life</li>
+                <li className="sidebarListItem">Music</li>
+                <li className="sidebarListItem">Style</li>
+                <li className="sidebarListItem">Sports</li>
+                <li className="sidebarListItem">Tech</li>
+                <li className="sidebarListItem">Cinema</li>
+            </ul>
+        </div>
+        <div className="sidebarItem">
+            <span className="sidebarTitle">FOLLOW US</span>
+            <div className="sidebarSocial">
+              <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+              <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+              <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+              <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+            </div>
+        </div>
+    </div>
+  )
+}
