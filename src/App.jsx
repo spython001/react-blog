0c87944eca8dbbd399353bf84b82a998a3f0b3f6
spython <emmanuelegbo01@gmail.com>
2023-09-15 18:19:30 +0100

    created a register page for registeration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/topbar";
-import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+//import Settings from "./pages/settings/Settings";
 //import Single from "./pages/single/Single";
 //import Write from "./pages/write/Write";
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Login />
     </>
   )
 }
