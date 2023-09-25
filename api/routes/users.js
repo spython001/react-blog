35b15880bffb2b822121502aeba88952abf30ab5
spython <emmanuelegbo01@gmail.com>
2023-09-25 15:51:17 +0100

    updated usersjs router file

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //UPDATE USER
@@ -12,7 +13,7 @@ router.put("/:id", async(req, res) => {
         try {
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set:req.body,
-            });
+            }, {new: true});
             res.status(200).json(updatedUser);
         } catch (err) {
             res.status(500).json(err);
@@ -23,6 +24,36 @@ router.put("/:id", async(req, res) => {
     
 });
 
-//DELETE
+//DELETE USER/POSTS
+router.delete("/:id", async(req, res) => {
+    if(req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({ username: user.username })
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted..");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } catch (err) {
+            res.status(404).json("User not found");
+        }
+        
+    } else {
+        res.status(401).json("You are allowed to delete only your account!");
+    }    
+});
+
+//GET 'ONE' USER
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
