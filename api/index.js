d3ed6105f5bfebd1d6ef10e8f769d85fd37f5a7c
spython <emmanuelegbo01@gmail.com>
2023-09-25 11:57:56 +0100

    mongo url

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use("/", (req, res)=> {
+    console.log('This is the boss');
+})
+
 
 app.listen("5000", () =>{
     console.log('Backend server is running');
