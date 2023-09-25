d2fc745092ed3fc44d9f019e901b0d509f5a6d57
spython <emmanuelegbo01@gmail.com>
2023-09-25 14:39:21 +0100

    modified files

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
+const morgan = require("morgan");
 const authRoute = require("./routes/auth");
 
 dotenv.config();
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL, {
 //MIDDLE-WARES
 app.use(express.json());
 app.use(helmet());
+app.use(morgan("common"))
 
 
 app.use("/api/auth", authRoute);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,4 +24,21 @@ router.post("/register", async(req, res) => {
     }
 })
 
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("wrong credentials");
+
+        //VALIDATING PASSWORD FOR UNIQUE USER
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("wrong credentials");
+        
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
