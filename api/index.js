57faa0164f293646d85db24e8c5b2aa2463c0e4b
spython <emmanuelegbo01@gmail.com>
2023-09-25 12:50:51 +0100

    created a Userjs file in model folde

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.use("/", (req, res)=> {
-    console.log('This is the boss');
-})
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {console.error(err);});
 
 app.listen("5000", () =>{
     console.log('Backend server is running');
-})
+});
