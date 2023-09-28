c5ff7838600ba876083a95f30da8c6583c076479
spython <emmanuelegbo01@gmail.com>
2023-09-28 15:11:10 +0100

    modified files

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   server: {
     host: true
   },
+  optimizeDeps: {
+    include: ['*.js'], // Include all .js files in your project
+  },
   esbuild: {
     loader: "jsx",
     include: [
